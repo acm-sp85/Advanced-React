b9ed9bdc80d2bd33323c88811185465def735302
Alex-new-laptop <acontell@gmail.com>
2022-05-11 08:56:07 -0400

    created a nav component

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import Nav from './Nav';
 
 export default function Header() {
   return (
@@ -12,6 +13,7 @@ export default function Header() {
       <div className="sub-bar">
         <p>Search</p>
       </div>
+      <Nav />
     </header>
   );
 }
