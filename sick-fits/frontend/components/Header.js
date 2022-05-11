c1ec9dced37d3d9bc126f552d02c6b52d5a3c522
Alex-new-laptop <acontell@gmail.com>
2022-05-11 09:47:57 -0400

    Added some global styles

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -1,19 +1,54 @@
 import Link from 'next/link';
 import Nav from './Nav';
+import styled from 'styled-components';
+
+const Logo = styled.h1`
+  font-size: 4rem;
+  margin-left: 2rem;
+  position: relative;
+  z-index: 2;
+  background: blue;
+  transform: skew(-6deg);
+  a {
+    color: white;
+    text-decoration: none;
+    text-transform: uppercase;
+    padding: 0.5rem 1 rem;
+  }
+`;
+
+const HeaderStyles = styled.header`
+  .bar {
+    border-bottom: 10px solid var(--black, black); //Here we are calling a color variable created for
+    //black, and stating a fallback regular black color
+    display: grid;
+    grid-template-columns: auto 1fr;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .sub-bar {
+    display: grid;
+    grid-template-columns: 1fr auto;
+    border-bottom: 1px solid var(--black, black);
+  }
+`;
 
 export default function Header() {
   return (
-    <header>
+    <HeaderStyles>
       <div className="bar">
         {/* Using this kind of link would make the page to hard refresh
         so we wouldn't be taking advantage of Next.js serverside rendering */}
         {/* <a href="/">RidOf</a> */}
-        <Link href="/">RidOf</Link>
+        <Logo>
+          <Link href="/">RidOF</Link>
+        </Logo>
       </div>
       <div className="sub-bar">
         <p>Search</p>
       </div>
       <Nav />
-    </header>
+    </HeaderStyles>
   );
 }
